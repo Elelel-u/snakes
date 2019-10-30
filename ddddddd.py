9bc096a0dbd895b8f3169166be55a95611f3d883
Elel <sonja.bolotova@yandex.ru>
2019-10-30 12:50:39 +0300

    thatsall :wq

---
+++ ddddddd.py
@@ -0,0 +1,17 @@
+import pygame
+pygame.init()
+screen=pygame.display.set_mode((600,500))
+done = False
+is_blue = True
+while not done:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            done = True
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            is_blue = not is_blue
+    pressed=pygame
+
+    if is_blue: color = (0,128, 255)
+    else: color = (255, 100, 0)
+    pygame.draw.rect(screen, color, pygame.Rect(30,30,60,60))
+    pygame.display.flip()
